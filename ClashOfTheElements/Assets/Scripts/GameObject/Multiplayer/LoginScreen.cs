c4ee65ee3377c8aea0008900aecbe2db9a9a4a48
Jonas Purtschert <jonaspu@student.ethz.ch>
2015-12-14 14:48:40 +0100

    Fixed main GUI scaling on Phone

--- ClashOfTheElements/Assets/Scripts/GameObject/Multiplayer/LoginScreen.cs
+++ ClashOfTheElements/Assets/Scripts/GameObject/Multiplayer/LoginScreen.cs
@@ -14,18 +14,37 @@ public class LoginScreen : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUI.Box (new Rect (0, 0, 1200, 900), "Menu");
 
-		if(GUI.Button(new Rect(525,200,150,50), "Start Singleplayer")){
+		// previously w=1200, h=900
+		int width = Screen.width;
+		int height = Screen.height;
+
+		// size of buttons
+		int buttonWidth = width / 5;
+		int buttonHeight = height / 10;
+
+		// get standard button height and width
+		int buttonX = width / 2 - buttonWidth / 2;
+		int buttonY = height / 6;
+
+		// create custom style for bigger font
+		GUIStyle customButton = new GUIStyle("button");
+		customButton.fontSize = 40;
+
+		// create gui box
+		GUI.Box (new Rect (0, 0, width, height));
+
+
+		if(GUI.Button(new Rect(buttonX,buttonY,buttonWidth,buttonHeight), "Start Singleplayer", customButton)){
 			Application.LoadLevel("testScene");
 		}
-		if(GUI.Button(new Rect(525,300,150,50), "Host Multiplayer")){
+		if(GUI.Button(new Rect(buttonX,buttonY*2,buttonWidth,buttonHeight), "Host Multiplayer", customButton)){
 			Application.LoadLevel("HostMultiplayer"); 
 		}
-		if(GUI.Button(new Rect(525,400,150,50), "Join Multiplayer")){
+		if(GUI.Button(new Rect(buttonX,buttonY*3,buttonWidth,buttonHeight), "Join Multiplayer", customButton)){
 			Application.LoadLevel("JoinMultiplayer"); 
 		}
-		if(GUI.Button(new Rect(525,500,150,50), "Options")){
+		if(GUI.Button(new Rect(buttonX,buttonY*4,buttonWidth,buttonHeight), "Options", customButton)){
 			Application.LoadLevel("Options"); 
 		}
 	}
